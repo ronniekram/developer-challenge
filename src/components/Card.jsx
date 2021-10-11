18c97a730585c98233357b45fda109d22b6b275d
Arynn Boniface <arynn.boniface@gmail.com>
2021-10-10 19:32:58 -0700

    Passing order button click event handler down to button component

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -27,12 +27,20 @@ const CardContainer = styled.div`
 
 // Passing down graphql data to display product info
 const Card = ({ description, image, quantity, title }) => {
+	// I'm not sure if this is okay as passing from high level component for order button event -- It made the most sense logically with the component setup.
+	const handleClick = () => {
+		console.log(`I ordered ${quantity.text} ${title.text}`);
+	};
+
 	return (
 		<CardContainer>
 			<CardImage image={image} title={title} />
 			<div className="product-info">
 				<MenuItem description={description} title={title} />
-				<Button quantity={quantity} />
+				<Button
+					quantity={quantity}
+					handleClick={handleClick}
+				/>
 			</div>
 		</CardContainer>
 	);
