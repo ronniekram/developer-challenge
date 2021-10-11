95df5ffb3e61b4e0b06752c6f5fc7c48b599da4c
Arynn Boniface <arynn.boniface@gmail.com>
2021-10-10 17:07:10 -0700

    Filling out button component

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,10 +1,33 @@
 import React from "react";
+import "normalize.css";
+import styled from "@emotion/styled";
 
-const Button = () => {
+const Container = styled.div`
+  margin: auto;
+  
+	button {
+		background-color: #da55bd;
+		color: #ffffff;
+
+		font-family: "Jura";
+		font-size: 1.5rem;
+		font-weight: 700;
+		text-transform: uppercase;
+
+		padding: 1rem 2.375rem;
+		border-radius: 1.25rem;
+    border: none;
+	}
+`;
+
+// passing in the quantity for button text
+const Button = ({ quantity, handleClick }) => {
   return (
-    <div>
-      
-    </div>
+    <Container>
+      <button type="button" onClick={handleClick}>
+				Order {quantity.text}
+			</button>
+    </Container>
   );
 }
 
